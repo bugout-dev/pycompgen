770ef4013a95eb5ae575a5e74d7ed2ad81d1624d
Neeraj Kashyap <neeraj@bugout.dev>
2023-09-10 20:59:59 -0700

    New test

--- pycompgen/test_inspect.py
+++ pycompgen/test_inspect.py
@@ -32,5 +32,32 @@ class TestInspectSimple(unittest.TestCase):
         )
 
 
+class TestInspectCommandWithSubcommandAndOptions(unittest.TestCase):
+    def setUp(self):
+        self.parser = argparse.ArgumentParser(
+            description="This is a parser to be used in tests"
+        )
+        self.parser.add_argument("-l", help="Something something lll")
+
+        subparsers = self.parser.add_subparsers()
+
+        subcommand_parser = subparsers.add_parser(
+            "a-subcommand", help="Something something subcommand"
+        )
+        subcommand_parser.add_argument(
+            "--suboption", required=True, help="Something something suboption"
+        )
+
+    def test_possible_completions_by_context(self):
+        completions = possible_completions_by_context(self.parser)
+        self.assertDictEqual(
+            completions,
+            {
+                tuple(): ["-h", "--help", "-l", "a-subcommand"],
+                ("a-subcommand",): ["-h", "--help", "--suboption"],
+            },
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
